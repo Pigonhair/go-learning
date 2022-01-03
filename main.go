0aa4ec8712adb967ecc00d6bd0000bef181aa1aa
김건우 <pigonhairtt@gmail.com>
2022-01-03 11:34:17 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func multiply(a, b int) int {
+	return a * b
+}
+
+func lenAndUpper(name string) (int, string) {
+	return len(name), strings.ToUpper(name)
+}
 
 func main() {
+	// name := "nico"
 	fmt.Println("HELLO WORLD")
+	fmt.Println(multiply(2, 2))
+	totalLength, _ := lenAndUpper("nico")
+	fmt.Println(totalLength)
 }
