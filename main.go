bc60ab93acf501d850e31fdd1db42c5d6c64d105
김건우 <pigonhairtt@gmail.com>
2022-01-11 14:13:38 +0900

    BANK & DICTIONARY PROJECTS FINISH
    BANK & DICTIONARY PROJECTS FINISH

--- main.go
+++ main.go
@@ -3,16 +3,21 @@ package main
 import (
 	"fmt"
 
-	"github.com/Pigonhair/go-learning/accounts"
+	"github.com/Pigonhair/go-learning/mydict"
 )
 
 func main() {
-	account := accounts.NewAccount("kunwoo")
-	account.Deposit(10)
-	fmt.Println(account.Balance())
-	err := account.Withdraw(20)
+	dictionary := mydict.Dictionary{}
+	baseWord := "hello"
+	dictionary.Add(baseWord, "First")
+	err := dictionary.Update(baseWord, "Second")
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(account.Balance())
+	dictionary.Search(baseWord)
+	fmt.Println(dictionary)
+	dictionary.Delete(baseWord)
+	word, _ := dictionary.Search(baseWord)
+	fmt.Println(dictionary)
+	fmt.Println(word)
 }
